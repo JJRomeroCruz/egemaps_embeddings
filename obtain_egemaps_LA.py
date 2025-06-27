e4a9c8761b593bfde864f08d510904a0f0d93d02
JJRomeroCruz <juanjo@DESKTOP-0K2SSFA.localdomain>
2025-06-27 19:06:25 +0200

    Randm Forest and SVM w egemaps

--- obtain_egemaps_LA.py
+++ obtain_egemaps_LA.py
@@ -4,11 +4,12 @@ import pandas as pd
 import numpy as np
 
 # sacamos los egemaps
-folder = "/home/juanjo/Documentos/eGeMAPS_embedding/ASVspoof2019_LA_train/flac"
+#folder_train = "/home/juanjo/Documentos/eGeMAPS_embedding/ASVspoof2019_LA_train/flac"
+folder_eval = "/home/juanjo/Documentos/eGeMAPS_embedding/ASVspoof2019_LA_eval/flac"
 
-df = egemaps.egemaps_from_folder(folder)
+df = egemaps.egemaps_from_folder(folder_eval)
 
 print(df.columns.to_list())
 
 # save the data
-df.to_csv("egemaps_LA.csv")
+df.to_csv("egemaps_LA_eval.csv")
