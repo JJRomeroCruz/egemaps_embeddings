43006e7d3bd4c4df81007773bdebf8b229bde58b
JJRomeroCruz <juanjo@DESKTOP-0K2SSFA.localdomain>
2025-07-22 11:30:19 +0200

    Experiments finished

--- models.py
+++ models.py
@@ -11,6 +11,14 @@ import metrics
 from sklearn.metrics import classification_report
 from sklearn.model_selection import cross_val_score
 
+from mlxtend.classifier import StackingClassifier
+from mlxtend.feature_selection import ColumnSelector
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import copy
+
 def concatenate_label_data(folder, label_file):
     list_df = []
     df = pd.DataFrame()
@@ -28,7 +36,7 @@ def concatenate_label_data(folder, label_file):
     print(len(df))
     return df
 
-def egemaps_svm(folder_train = "/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv", folder_eval):
+def egemaps_svm(folder_eval, folder_train = "/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv"):
     # load data
     cols_gmaps = ['F0semitoneFrom27.5Hz_sma3nz_amean', 'F0semitoneFrom27.5Hz_sma3nz_stddevNorm', 'F0semitoneFrom27.5Hz_sma3nz_percentile20.0', 'F0semitoneFrom27.5Hz_sma3nz_percentile50.0', 'F0semitoneFrom27.5Hz_sma3nz_percentile80.0', 'F0semitoneFrom27.5Hz_sma3nz_pctlrange0-2', 'F0semitoneFrom27.5Hz_sma3nz_meanRisingSlope', 'F0semitoneFrom27.5Hz_sma3nz_stddevRisingSlope', 'F0semitoneFrom27.5Hz_sma3nz_meanFallingSlope', 'F0semitoneFrom27.5Hz_sma3nz_stddevFallingSlope', 'loudness_sma3_amean', 'loudness_sma3_stddevNorm', 'loudness_sma3_percentile20.0', 'loudness_sma3_percentile50.0', 'loudness_sma3_percentile80.0', 'loudness_sma3_pctlrange0-2', 'loudness_sma3_meanRisingSlope', 'loudness_sma3_stddevRisingSlope', 'loudness_sma3_meanFallingSlope', 'loudness_sma3_stddevFallingSlope', 'spectralFlux_sma3_amean', 'spectralFlux_sma3_stddevNorm', 'mfcc1_sma3_amean', 'mfcc1_sma3_stddevNorm', 'mfcc2_sma3_amean', 'mfcc2_sma3_stddevNorm', 'mfcc3_sma3_amean', 'mfcc3_sma3_stddevNorm', 'mfcc4_sma3_amean', 'mfcc4_sma3_stddevNorm', 'jitterLocal_sma3nz_amean', 'jitterLocal_sma3nz_stddevNorm', 'shimmerLocaldB_sma3nz_amean', 'shimmerLocaldB_sma3nz_stddevNorm', 'HNRdBACF_sma3nz_amean', 'HNRdBACF_sma3nz_stddevNorm', 'logRelF0-H1-H2_sma3nz_amean', 'logRelF0-H1-H2_sma3nz_stddevNorm', 'logRelF0-H1-A3_sma3nz_amean', 'logRelF0-H1-A3_sma3nz_stddevNorm', 'F1frequency_sma3nz_amean', 'F1frequency_sma3nz_stddevNorm', 'F1bandwidth_sma3nz_amean', 'F1bandwidth_sma3nz_stddevNorm', 'F1amplitudeLogRelF0_sma3nz_amean', 'F1amplitudeLogRelF0_sma3nz_stddevNorm', 'F2frequency_sma3nz_amean', 'F2frequency_sma3nz_stddevNorm', 'F2bandwidth_sma3nz_amean', 'F2bandwidth_sma3nz_stddevNorm', 'F2amplitudeLogRelF0_sma3nz_amean', 'F2amplitudeLogRelF0_sma3nz_stddevNorm', 'F3frequency_sma3nz_amean', 'F3frequency_sma3nz_stddevNorm', 'F3bandwidth_sma3nz_amean', 'F3bandwidth_sma3nz_stddevNorm', 'F3amplitudeLogRelF0_sma3nz_amean', 'F3amplitudeLogRelF0_sma3nz_stddevNorm', 'alphaRatioV_sma3nz_amean', 'alphaRatioV_sma3nz_stddevNorm', 'hammarbergIndexV_sma3nz_amean', 'hammarbergIndexV_sma3nz_stddevNorm', 'slopeV0-500_sma3nz_amean', 'slopeV0-500_sma3nz_stddevNorm', 'slopeV500-1500_sma3nz_amean', 'slopeV500-1500_sma3nz_stddevNorm', 'spectralFluxV_sma3nz_amean', 'spectralFluxV_sma3nz_stddevNorm', 'mfcc1V_sma3nz_amean', 'mfcc1V_sma3nz_stddevNorm', 'mfcc2V_sma3nz_amean', 'mfcc2V_sma3nz_stddevNorm', 'mfcc3V_sma3nz_amean', 'mfcc3V_sma3nz_stddevNorm', 'mfcc4V_sma3nz_amean', 'mfcc4V_sma3nz_stddevNorm', 'alphaRatioUV_sma3nz_amean', 'hammarbergIndexUV_sma3nz_amean', 'slopeUV0-500_sma3nz_amean', 'slopeUV500-1500_sma3nz_amean', 'spectralFluxUV_sma3nz_amean', 'loudnessPeaksPerSec', 'VoicedSegmentsPerSec', 'MeanVoicedSegmentLengthSec', 'StddevVoicedSegmentLengthSec', 'MeanUnvoicedSegmentLength', 'StddevUnvoicedSegmentLength', 'equivalentSoundLevel_dBp']
     df_train = pd.read_csv(folder_train)
@@ -52,11 +60,11 @@ def egemaps_svm(folder_train = "/home/juanjo/Documentos/eGeMAPS_embedding/egemap
 
     return y_pred, y_scores, y_eval 
 
-def wav2vec2_svm(train = ["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval):
-    folder_train = train[0]
-    label_train = train[1]
-    folder_eval = eval[0]
-    label_eval = eval[1]
+def wav2vec2_svm(eval, train = ["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_LA_train"]):
+    folder_train = train[1]
+    label_train = train[0]
+    folder_eval = eval[1]
+    label_eval = eval[0]
     
     # load data
     df_train = concatenate_label_data(
@@ -99,7 +107,7 @@ def wav2vec2_svm(train = ["egemaps_LA_train_labeled.csv", "embeddings_LA_train"]
 
     return y_pred, y_scores, y_eval
 
-def concatenation_svm(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval):
+def concatenation_svm(eval, train=["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_LA_train"]):
 
     folder_train_egemaps = train[0]
     folder_train_em = train[1]
@@ -114,6 +122,8 @@ def concatenation_svm(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_trai
     folder_train_em, folder_train_egemaps
     )
     df1_train = df1_train.sort_values(by="audio").reset_index(drop=True)
+    df1_train["audio"] = df1_train["audio"].apply(lambda x: x.replace(".wav", ""))
+
     features_df = pd.DataFrame(
     df1_train["embedding"].tolist(),
     index=df1_train.index).add_prefix("feat_")
@@ -143,7 +153,7 @@ def concatenation_svm(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_trai
     df1_eval = pd.concat([df1_eval, features_df], axis=1)
 
     df2_eval = pd.read_csv(folder_eval_egemaps)
-    df2_train["audio"] = df2_train["audio"].apply(lambda x: x.replace(".wav", ""))
+    df2_eval["audio"] = df2_eval["audio"].apply(lambda x: x.replace(".wav", ""))
 
     df_eval = pd.merge(df1_eval, df2_eval, on="audio")
 
@@ -164,7 +174,7 @@ def concatenation_svm(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_trai
 
     return y_pred, y_scores, y_eval
 
-def late_fusion(train = ["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval):
+def late_fusion(eval, train = ["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_LA_train"]):
 
     res1 = egemaps_svm(folder_train=train[0], folder_eval=eval[0])
     res2 = wav2vec2_svm(train=train, eval=eval)
@@ -173,7 +183,7 @@ def late_fusion(train = ["egemaps_LA_train_labeled.csv", "embeddings_LA_train"],
     y_pred = [0 if x < 0.5 else 1 for x in y_scores]
     return y_pred, y_scores, y_eval
 
-def stacking(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval):
+def stacking(eval, train=["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_LA_train_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_LA_train"]):
 
     folder_train_egemaps = train[0]
     folder_train_em = train[1]
@@ -217,7 +227,7 @@ def stacking(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval
     df1_eval = pd.concat([df1_eval, features_df], axis=1)
 
     df2_eval = pd.read_csv(folder_eval_egemaps)
-    df2_train["audio"] = df2_train["audio"].apply(lambda x: x.replace(".wav", ""))
+    df2_eval["audio"] = df2_eval["audio"].apply(lambda x: x.replace(".wav", ""))
 
     df_eval = pd.merge(df1_eval, df2_eval, on="audio")
 
@@ -249,7 +259,7 @@ def stacking(train=["egemaps_LA_train_labeled.csv", "embeddings_LA_train"], eval
 
     # evaluate the model
     y_scores = sclf.decision_function(x_eval)
-    print(y_score)
+    print(y_scores)
     y_pred = [0 if x < 0.5 else 1 for x in y_scores]
 
     return y_pred, y_scores, y_eval
--- plot_ROC.py
+++ plot_ROC.py
@@ -1,2 +1,35 @@
 import matplotlib.pyplot as plt
-import 
+import models
+import seaborn as sns
+from sklearn.metrics import roc_curve, auc, accuracy_score, confusion_matrix
+import metrics
+
+def plot_roc_models(eval, train=["egemaps_LA_train_labeled.csv", "embeddings_LA_train"]):
+    
+    # sacamos los y_pred, y_scores, y_eval de cada modelo
+    res = [models.egemaps_svm(folder_eval=eval[0]), 
+           models.wav2vec2_svm(eval=eval), 
+           models.concatenation_svm(eval=eval), models.late_fusion(eval=eval),
+           models.stacking(eval=eval)]
+    
+    titulos = ["eGeMAPS", "wav2vec2", "Early Fusion", "Late Fusion", "Stacking"]
+
+    plt.figure(figsize=(16, 12))
+    for i in range(len(res)):
+        y_true, y_scores = res[i][2], res[i][1]
+        fpr, tpr, thres = roc_curve(y_true, y_scores)
+        plt.plot(fpr, tpr, label=titulos[i])
+    
+    plt.plot([0, 1], [0, 1], color='red', lw=1, linestyle="--")
+    plt.xlabel("Tasa de falsos positivos (FPR)")
+    plt.ylabel("Tasa de verdaderos positivos (TPR)")
+    plt.title("Curva ROC HABLA")
+    plt.legend(loc="lower right")
+    plt.grid(True)
+    plt.show()
+    
+    return
+
+#plot_roc_models(eval=["egemaps_LA_eval_labeled.csv", "embeddings_LA_eval"])
+#plot_roc_models(eval=["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_wild_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_WILD"])
+plot_roc_models(eval=["/home/juanjo/Documentos/eGeMAPS_embedding/egemaps_HABLA_labeled.csv", "/home/juanjo/Documentos/eGeMAPS_embedding/embeddings_HABLA"])
